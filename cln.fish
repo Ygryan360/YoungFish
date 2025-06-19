3dc422c03afbb72d00534ad7e2ebb2d7ef8ab323
Rayane Tchabodi <rayanetchabodi360@gmail.com>
2025-06-19 06:55:02 +0000

    add logs & fix dnf clean

--- cln.fish
+++ cln.fish
@@ -1,4 +1,4 @@
 function cln --wraps='sudo dnf clean' --description 'alias de sudo dnf clean'
-  sudo dnf clean $argv
+  sudo dnf clean all $argv
         
 end
---
+++ logs.fish
@@ -0,0 +1,4 @@
+function logs --wraps='cd ~/.logs/' --description 'alias logs=cd ~/.logs/'
+  cd ~/.logs/ $argv
+        
+end
