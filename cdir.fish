c3d9ab17546734bb205583fdf857b64e71d933b2
Rayane Tchabodi <rayanetchabodi360@gmail.com>
2025-09-08 08:32:58 +0000

    add cdir and gmain functions for directory creation and aliasing

---
+++ cdir.fish
@@ -0,0 +1,5 @@
+function cdir --description 'fonction pour créer un répertoire et y accéder'
+  mkdir $argv[1]
+  cd $argv[1]
+
+end
---
+++ gmain.fish
@@ -0,0 +1,4 @@
+function gmain --wraps='gcheck main' --description 'alias gmain=gcheck main'
+  gcheck main $argv
+        
+end
